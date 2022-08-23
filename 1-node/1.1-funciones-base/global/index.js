921b3a4618eb82e4611edaec18fa069d58809103
rusanrod <rsandovr@gmail.com>
2022-08-22 19:04:45 -0500

    feat: agrega funciones de global y os

--- 1-node/1.1-funciones-base/global/index.js
+++ 1-node/1.1-funciones-base/global/index.js
@@ -1,3 +1,6 @@
 console.log(this) //{} o module.exports
 console.log('Ruta donde esta mi archivo', __dirname)
 console.log('Ruta + nombre de mi archivo', __filename)
+//module: Referencia al modulo actual
+//export: Para exportar modulos (archivos)
+//require(): Importa un archivo js como modulo. require('index.js')
--- 1-node/1.1-funciones-base/os/index.js
+++ 1-node/1.1-funciones-base/os/index.js
@@ -1,3 +1,13 @@
 const os = require('os')
 
 console.log('version del sistema: ', os.version())
+
+console.log('Arquitectura: ', os.arch())
+console.log('Da información acerca de los cpu s: ',os.cpus())
+console.log('Número de procesadores lógicos: ', os.cpus().length)
+console.log('Memoria libre en bytes: ', os.freemem())
+console.log('Carpeta del usuario actual: ', os.homedir())
+console.log('Mi dirección IP o mi hostname (local):', os.hostname())
+console.log('Nombre de la versión del sistema: ', os.platform())
+console.log('Número de la versión del sistema: ', os.release())
+console.log('Versión del sistema detallada: ', os.version())
